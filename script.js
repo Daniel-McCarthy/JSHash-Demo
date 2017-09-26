668a3cc50481de04ef4fe8e46765ad78cebd8853
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-25 21:44:00 -0500

    JS: Added checkInput() Function
    This function takes the current input and compares it with the previous
    accepted input. If it detects a difference it saves the current input
    and calls the hashInput() function.

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function startTimer()
 	setInterval(checkInput, 250);
 }
 
+function checkInput()
+{
+	var input = document.getElementById("hashInput").value;
+	
+	if(input != lastInput)
+	{
+		lastInput = input;
+		hashInput();
+	}
+}
+
