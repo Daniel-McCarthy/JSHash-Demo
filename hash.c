2b284cf0fa0a32117955361f785e5566fb0a9287
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-26 15:43:46 -0500

    C: Finished addition of write/readMemory Functions

--- hash.c
+++ hash.c
@@ -1,9 +1,11 @@
 unsigned char* memory = 0;
 
+void writeMemory(int offset, unsigned char value)
 {
   *(memory + offset) = value;
 }
 
+unsigned int readMemory(int offset)
 {
   return *(memory + offset);
 }
