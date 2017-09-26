71d131b9a52e94ac6b31fb237bbd93cafec865d2
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-26 15:44:08 -0500

    C: Addition of FNV0 Hash Implementation

--- hash.c
+++ hash.c
@@ -9,3 +9,17 @@ unsigned int readMemory(int offset)
 {
   return *(memory + offset);
 }
+
+int fnv0(int messageLength)
+{
+	
+  int hash = 0;
+  
+  for(int i = 0; i < messageLength; i++)
+  {
+    hash *= 16777619;
+    hash ^= readMemory(i);
+  }
+  
+  return hash;
+}
