9134738a6454c56636e494500dbbbabdec60fc4d
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-26 15:48:25 -0500

    JS: Addition of HashInput Function
    Added function which will be the basis for all the hash operations. It
    uses web assembly to interact with C compiled hash implementations. It
    finds the current selected algorithm and hashes text in the input box
    and outputs the result to the output box.

--- script.js
+++ script.js
@@ -16,3 +16,45 @@ function checkInput()
 	}
 }
 
+function hashInput()
+{
+	var algorithm = document.getElementById("algorithmDropDownMenu").value;
+	var input = document.getElementById("hashInput").value;
+
+	fetch('hash.wasm').then(response =>
+	  response.arrayBuffer()
+	).then(bytes =>
+	  WebAssembly.instantiate(bytes)
+	).then(results => {
+
+		var wasm = results.instance.exports;
+		let memory = new Uint32Array(wasm.memory.buffer);
+		
+		const readMemory = wasm.readMemory;
+		const writeMemory = wasm.writeMemory;
+		
+		if(algorithm == "FNV-0")
+		{
+			const fnv0 = wasm.fnv0;
+
+			var input = document.getElementById("hashInput").value;
+
+			for(var i = 0; i < input.length; i++)
+			{
+			  writeMemory(i, input.charCodeAt(i));
+			}
+			
+			var hash = fnv0(10).toString(16);
+			document.getElementById("hashOutput").value = hash;
+		}
+		else if(algorithm == "FNV-1")
+		{
+			
+		}
+		else if(algorithm == "FNV-1A")
+		{
+			
+		}
+
+	});
+}
