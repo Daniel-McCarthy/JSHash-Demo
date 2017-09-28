03fe341f1c0e2d86199e65f742514857297ec428
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-27 21:49:24 -0500

    JS: HashInput() Added Call to JS Implementation of FNV0

--- script.js
+++ script.js
@@ -35,7 +35,15 @@ function hashInput()
 		{
 			case "FNV-0":
 			{
-			
+				var hash = fnv0(input);
+				var hashString = hash.toString(16);
+				
+				if (hashString.charAt (0) == '-')
+				{
+					hashString = hashString.substring(1);
+				}
+				
+				document.getElementById("hashOutput").value = hashString;
 				break;
 			}
 			case "FNV-1":
