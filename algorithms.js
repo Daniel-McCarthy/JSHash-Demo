3efc2e0fc8cf441c68ee74af76b97a7926fab30a
Dan McCarthy <wererabbbit@yahoo.com>
2017-09-27 21:50:33 -0500

    JS: Added implementation of FNV1A

--- algorithms.js
+++ algorithms.js
@@ -24,3 +24,16 @@ function fnv1(message)
 	
 	return hash | 0;
 }
+
+function fnv1a(message)
+{
+	var hash = 0x811C9DC5
+	
+	for(var i = 0; i < message.length; i++)
+	{
+		hash =  (hash ^ message.charCodeAt(i));
+		hash = Math.imul(hash, 16777619);
+	}
+	
+	return hash | 0;
+}
