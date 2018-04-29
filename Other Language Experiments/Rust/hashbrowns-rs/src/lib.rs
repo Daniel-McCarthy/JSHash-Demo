d63c01873ab7fd9fb9c98c9ce1fd6bca6e8c71a4
Dan McCarthy <wererabbbit@yahoo.com>
2018-04-28 21:37:37 -0500

    Added CRC32 Hash Algorithms in Rust

--- Other Language Experiments/Rust/hashbrowns-rs/src/lib.rs
+++ Other Language Experiments/Rust/hashbrowns-rs/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod fnv;
 pub mod adler;
+pub mod crc;
 pub mod reverse_endian;
 
 #[cfg(test)]
@@ -24,6 +25,16 @@ mod tests {
 	#[test]
     fn adler32_test() {
         assert_eq!( format!( "{:x}", adler::adler32("HelloKitty") ), "152d040a");
+    }
+	
+	#[test]
+    fn crc_32_test() {
+        assert_eq!( format!( "{:x}", crc::crc_32("HelloKitty") ), "9a133de9");
+    }
+	
+	#[test]
+    fn crc_b_32_test() {
+        assert_eq!( format!( "{:x}", crc::crc_b_32("HelloKitty") ), "129ca114");
     }
 	
 
